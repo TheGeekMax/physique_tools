9ef971ecc056f3d9ef62c22e85ab67f49819e510
TheGeekMax <maximesanciaume@gmail.com>
2021-11-19 23:38:13 +0100

    ajout derrivee et readme

---
+++ README.md
@@ -0,0 +1,34 @@
+#fonctions disponibles
+
+##derivee(f,x)
+permet de derriver une fonction f , en un point x
+
+f -> fonction du type f(n)
+x -> entier
+
+##primitive(f,x)
+permet de trouver la primitive de f en x
+
+f -> fonction du type f(n)
+x -> entier
+
+##integrale(f,a,b)
+permet d'integrer une fonction f entre a et b
+
+f -> fonction du type f(n)
+a -> entier
+b -> entier
+
+##derivee_tableau(f,h,tab):
+permet de derriver une fonction f en fonction des valeurs d'un tableau
+
+f -> fonction du type f(n)
+h -> entier
+tab -> tableau des valeurs (de preference crée par la fonction linspace)
+
+##primitive_tableau(f,h,tab)
+permet de trouver la primitive de f en fonction des valeurs d'un tableau
+
+f -> fonction du type f(n)
+h -> entier
+tab -> tableau des valeurs (de preference crée par la fonction linspace)
--- derrivee.py
+++ derrivee.py
@@ -1,4 +1,6 @@
 from math import *
+from numpy import *
+from matplotlib.pyplot import *
 
 #fonction qui permet de faire automatiquement la dérivée d'une fonction
 def derivee(f,x):
@@ -14,6 +16,29 @@ def primitive(f,x):
         s+=h*f(h*i)
     return s
 
+def integrale(f,a,b):
+    return primitive(f,b)-primitive(f,a)
 
-#primitive de la fonction 2x en 5
-print(primitive(lambda x: 2*x,5))
+#fonction qui derrive un tableau par rapport a une fonction avec un pas de h
+def derivee_tableau(f,h,tab):
+    #on cree un tableau de la meme taille que le tableau d'entree
+    tab_derivee=[]
+    for i in range(0,len(tab)):
+        tab_derivee.append(0)
+    #on parcourt le tableau d'entree
+    for i in range(0,len(tab)):
+        #on calcule la derivee de la fonction
+        tab_derivee[i]=derivee(f,tab[i])
+    return tab_derivee
+
+#fonction qui fait la primitive d'un tableau par rapport a une fonction avec un pas de h
+def primitive_tableau(f,h,tab):
+    #on cree un tableau de la meme taille que le tableau d'entree
+    tab_primitive=[]
+    for i in range(0,len(tab)):
+        tab_primitive.append(0)
+    #on parcourt le tableau d'entree
+    for i in range(0,len(tab)):
+        #on calcule la primitive de la fonction
+        tab_primitive[i]=primitive(f,tab[i])
+    return tab_primitive
